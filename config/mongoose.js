09091c716290efc702591bdfff84b9376947f454
asif malik <aasif2364@gmail.com>
2023-05-03 23:46:00 +0530

    connecting to db

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,16 +1,15 @@
 const mongoose = require("mongoose");
  
-const url = process.env.MONGO_URI;
-console.log("i am " , url);
+
 const connectDb = async () => {
   try {
     mongoose.set("strictQuery", false);
     await mongoose.connect(
-      url
+      "mongodb+srv://asifMalik:lEWvxkc6TOdg6c2Z@mydb.ijjdxjc.mongodb.net/?retryWrites=true&w=majority"
     );
     console.log("Connected to DB Successfully");
   } catch (error) {
-    console.log("Error in Connecting to the DB");
+    console.log("Error in Connecting to the DB" , error);
   }
 };
 
--- routes/product.js
+++ routes/product.js
@@ -1,22 +1,28 @@
 const express = require("express");
-const {verifyToken , verifyTokenAdmin} = require("../middlewares/verifyToken");
-const {addProduct , getAllProduct , getSingleProduct, updateProduct , deleteProduct} = require("../controllers/Product_Controller");
+const { verifyToken, verifyTokenAdmin } = require("../middlewares/verifyToken");
+const {
+  addProduct,
+  getAllProduct,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/Product_Controller");
 
 const router = express.Router();
 
 // add product
-router.post("/add" , verifyToken , verifyTokenAdmin , addProduct);
+router.post("/add", addProduct);
 
 // delete product
-router.delete("/:id" , verifyToken , verifyTokenAdmin , deleteProduct);
+router.delete("/:id", verifyToken, verifyTokenAdmin, deleteProduct);
 
 // update product
-router.put("/:id" , verifyToken , verifyTokenAdmin , updateProduct);
+router.put("/:id", verifyToken, verifyTokenAdmin, updateProduct);
 
 // get all product
-router.get("/all" , getAllProduct);
+router.get("/all", getAllProduct);
 
 // get single product
-router.get("/:id" , getSingleProduct);
+router.get("/:id", getSingleProduct);
 
-module.exports=router;
+module.exports = router;
